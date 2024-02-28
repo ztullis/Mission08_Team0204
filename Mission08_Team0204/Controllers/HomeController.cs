9cd961992ddb84bce6f17b8536e93e247b6a0f24
Zach Tullis <zachjtullis@gmail.com>
2024-02-28 13:56:57 -0700

    Created all the HomeController Actions. Still need to coordinate with the team to make sure variables and action names are in sync.

--- Mission08_Team0204/Controllers/HomeController.cs
+++ Mission08_Team0204/Controllers/HomeController.cs
@@ -6,13 +6,81 @@ namespace Mission08_Team0204.Controllers
 { //Testing
     public class HomeController : Controller
     {
-        public HomeController()
+        private ITaskRepository _repo;
+        public HomeController(ITaskRepository temp)
         {
+            _repo = temp;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var allTasks = _repo.GetAllTasks();
+
+            return View(allTasks);
+        }
+
+        [HttpGet]
+        public IActionResult AddTask()
+        {
+            ViewBag.CategoryViewBag = _repo.Categories
+                .OrderBy(x => x.CategoryName)
+                .ToList();
+
+            return View(new Task());
+        }
+
+        [HttpPost]
+        public IActionResult AddTask(Task response)
+        {
+            if ModelState.IsValid)
+            {
+                _repo.AddTask(response);
+            }
+            else
+            {
+                ViewBag.CategoryViewBag = _repo.Categories
+                    .OrderBy(x => x.CategoryName)
+                    .ToList();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var recordToEdit = _repo.GetTask(id);
+
+            return View("AddTask", recordToEdit);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Task updatedTask)
+        {
+            if ModelState.IsValid)
+            {
+                _repo.EditTask(response);
+            }
+            else
+            {
+                ViewBag.CategoryViewBag = _repo.Categories
+                    .OrderBy(x => x.CategoryName)
+                    .ToList();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var taskToDelete = _repo.GetTask(id);
+
+            return View(taskToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Task taskToDelete)
+        {
+            _repo.DeleteTask(taskToDelete);
+
+            return RedirectToAction("Index");
         }
     }
 }
