d964fa7c795e54558ec174f73a4bd6966610efee
ryanward <ryanward@RYANWARDCE62>
2024-02-29 11:29:22 -0700

    Ryan Branch Push

---
+++ Mission08_Team0204/Models/EFTaskRepository.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.EntityFrameworkCore.ValueGeneration.Internal;
+using SQLitePCL;
+
+namespace Mission08_Team0204.Models
+{
+    public class EFTaskRepository : ITaskRepository
+    {
+        private TaskContext _context;
+        
+        public EFTaskRepository(TaskContext temp)
+        {
+            _context = temp;
+        }
+
+        public List<Task> Tasks => _context.Tasks.ToList();
+
+        public void AddTask(Task task)
+        {
+            _context.Tasks.Add(task);
+            _context.SaveChanges();
+        }
+
+        public void DeleteTask(Task task)
+        {
+            _context.Tasks.Remove(task);
+            _context.SaveChanges();
+
+        }
+
+        public void EditTask(Task task)
+        {
+            _context.Tasks.Update(task);
+            _context.SaveChanges();
+        }
+    }
+}
